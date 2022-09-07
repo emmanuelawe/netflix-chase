f690c8e5fe826ee813b953e65d7d1cefc12dcc56
emmanuelawe <aweemmanuelayobami@gmail.com>
2022-09-07 20:58:13 +0100

    Homepage UI done

--- pages/components/Banner.tsx
+++ pages/components/Banner.tsx
@@ -26,7 +26,8 @@ console.log(movie)
         <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
         layout="fill" objectFit="cover"/>
         </div>
-<div className="pt-16 space-y-2">
+
+<div className="pt-16 space-y-4">
 <h1 className="text-2xl font-bold md:text-3xl lg:text-6xl ">{movie?.title || movie?.name || movie?.original_name}</h1>
         <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
         <div className="flex space-x-3">
