1a1ecb2615d331b647cd193f944ada4a57562c6f
emmanuelawe <aweemmanuelayobami@gmail.com>
2022-09-08 20:24:28 +0100

    New

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -33,8 +33,13 @@ console.log(movie)
         layout="fill" objectFit="cover"/>
         </div>
 
+<<<<<<< HEAD
 <div className="pt-16 space-y-4 ">
 <h1 className="text-2xl lg:pt-40 font-bold md:text-3xl lg:text-6xl">{movie?.title || movie?.name || movie?.original_name}</h1>
+=======
+<div className="pt-16 space-y-4">
+<h1 className="text-2xl lg:pt-40 font-bold md:text-3xl lg:text-6xl ">{movie?.title || movie?.name || movie?.original_name}</h1>
+>>>>>>> 64f739a162005bd82d295332be1e17a4611edbc8
         <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
         <div className="flex space-x-3">
             <button className="bannerButton bg-white text-black"><FaPlay className="h-4 w-4 text-black md:h-7 md:w-7"/> Play</button>
@@ -48,5 +53,6 @@ console.log(movie)
     </div>
   )
 }
+
 
 export default Banner
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
 import { Movie } from '../typings'
+<<<<<<< HEAD
 import {useRecoilState} from 'recoil'
 import {modalState, movieState} from '../atoms/modalAtom'
+=======
+>>>>>>> 64f739a162005bd82d295332be1e17a4611edbc8
 
 interface Props {
     movie: Movie 
